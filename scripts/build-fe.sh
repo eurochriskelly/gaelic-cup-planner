de6f19292d954552d1026441860e48f7049d0205
Chris Kelly <eurochriskelly@gmail.com>
2023-10-24 01:31:35 +0200

    Added a templating system for sharing resources amongst front end pages
    - Site css can be consistent
    - Frontend JS libraries reusable
    - Builds to app directory as a single page document
    - Added google app scripts mocking capability

---
+++ scripts/build-fe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e 
+
+TOP_DIR=$(pwd)
+echo "TOP_DIR: $TOP_DIR"
+cd src/frontend/interfaces/huddle
+ls
+test -d dist && rm -rf dist
+mkdir dist
+echo "Building front end"
+node esbuild.config.js
+ls -alh $TOP_DIR/src/frontend/mock
+head -n 10 $TOP_DIR/src/frontend/mock/teams.html
