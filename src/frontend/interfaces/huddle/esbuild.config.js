0e7c2509b89d7361d0795ddb9d369c5ec0371de1
eurochriskelly@gmail.com <eurochriskelly@gmail.com>
2023-10-24 23:09:29 +0200

    Create a custom build tool for all frontends in the system

--- src/frontend/interfaces/huddle/esbuild.config.js
+++ src/frontend/interfaces/huddle/esbuild.config.js
@@ -1,52 +1,16 @@
-const fs = require('fs');
+
+const { renderWebPage } = require('../../shared/renderer')
 
 const UI_INFO = {
   title: 'Team Huddle',
+  files: {
+    css: [
+      '../../shared/site.css'
+    ]
+  }
 }
 
-const cssSite = fs.readFileSync('../../shared/site.css', 'utf8');
-const cssData = fs.readFileSync('style.css', 'utf8');
-const jsData = fs.readFileSync('main.js', 'utf8');
-const htmlData = `
-  <!DOCTYPE html>
-    <html>
-    <head>
-        <base target="_top">
-        <title>${UI_INFO.title}</title>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="./lib/google-mock.js"></script>
-        <script>
-            ${jsData}
-        </script>
-        <style>
-            ${cssSite}
-        </style>
-        <style>
-            ${cssData}
-        </style>
-    </head>
-    <body>
-      <h1>${UI_INFO.title}</h1>
-      <article id="content">
-        <section id="sec-select">
-          <h2>Select your team ...</h2>
-          <div id="selectors">
-            <select id="gender-selector">
-                <option value="" disabled selected>Select Gender</option>
-            </select>
-            <select id="level-selector">
-                <option value="" disabled selected>Select Level</option>
-            </select>
-            <select id="team-selector">
-                <option value="" disabled selected>Select Team</option>
-            </select>
-          </div>
-        </section>
-        <section id="sec-main"></section>
-      </article>
-    </body>
-    </html>
-  `;
+renderWebPage(UI_INFO);
 
-  fs.writeFileSync('../../mock/teams.html', htmlData);
+
 
