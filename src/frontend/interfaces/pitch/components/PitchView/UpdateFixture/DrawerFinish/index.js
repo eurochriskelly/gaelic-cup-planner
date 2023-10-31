d4870bce2c710d60b82fcbc8ad2058bbed9f243a
Chris Kelly <eurochriskelly@gmail.com>
2023-10-31 17:16:29 +0100

    Adding a point setting drawer

--- src/frontend/interfaces/pitch/components/PitchView/UpdateFixture/DrawerFinish/index.js
+++ src/frontend/interfaces/pitch/components/PitchView/UpdateFixture/DrawerFinish/index.js
@@ -3,7 +3,7 @@ import styles from './DrawerFinish.module.css'
 const DrawerFinish = ({ fixture, onClose, onConfirm }) => {
     const { Team1, Team2 } = fixture
     return <div className={styles.container}>
-        <h3>Update match score</h3>
+        <div className={styles.drawerHead}>Update match score</div>
         <div className={styles.teamScore}>
         <h4>{Team1}</h4>
             <div>
@@ -22,7 +22,7 @@ const DrawerFinish = ({ fixture, onClose, onConfirm }) => {
             <button onClick={onClose}>Cancel</button>
             <button onClick={onConfirm}>Confirm</button>
         </div>
-        <h3>Carded players?</h3>
+        <div className="drawerHead">Carded players?</div>
         <div>
             <button>Yes</button>
             <button>No</button>
