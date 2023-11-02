9556e630b6c6e4504e53193146963a64602b949b
Chris Kelly <chris.kelly@progress.com>
2023-11-02 15:58:43 +0100

    Udate sub-dialogs for field coordination

--- src/frontend/interfaces/pitch/components/PitchView/UpdateFixture/DrawerFinish/index.js
+++ src/frontend/interfaces/pitch/components/PitchView/UpdateFixture/DrawerFinish/index.js
@@ -3,29 +3,34 @@ import styles from './DrawerFinish.module.css'
 const DrawerFinish = ({ fixture, onClose, onConfirm }) => {
     const { Team1, Team2 } = fixture
     return <div className={styles.container}>
-        <div className={styles.drawerHead}>Update match score</div>
-        <div className={styles.teamScore}>
-        <h4>{Team1}</h4>
+        <div className="drawer-header">Update match score</div>
+        <div className="drawer-container">
+            <div className={styles.teamScore}>
+                <h4>{Team1}</h4>
+                <div>
+                    <input type="number" placeholder="Goals" />
+                    <input type="number" placeholder="Points" />
+                </div>
+            </div>
+            <div className={styles.teamScore}>
+                <h4>{Team2}</h4>
+                <div>
+                    <input type="number" placeholder="Goals" />
+                    <input type="number" placeholder="Points" />
+                </div>
+            </div>
             <div>
-                <input type="number" placeholder="Goals" />
-                <input type="number" placeholder="Points" />
+                <button className="enabled" onClick={onConfirm}>Proceed</button>
+                <button onClick={onClose}>Cancel</button>
             </div>
         </div>
-        <div className={styles.teamScore}>
-            <h4>{Team2}</h4>
-            <div>
-                <input type="number" placeholder="Goals" />
-                <input type="number" placeholder="Points" />
+
+        <div>
+            <div className="drawer-header">Carded players?</div>
+            <div className="drawer-container">
+                <button>Yes</button>
+                <button>No</button>
             </div>
-        </div>
-        <div>
-            <button onClick={onClose}>Cancel</button>
-            <button onClick={onConfirm}>Confirm</button>
-        </div>
-        <div className="drawerHead">Carded players?</div>
-        <div>
-            <button>Yes</button>
-            <button>No</button>
         </div>
     </div>
 }
