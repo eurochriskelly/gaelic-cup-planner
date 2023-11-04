1f9c25a7973a5223642058c8069bded092f8c2a1
eurochriskelly@gmail.com <eurochriskelly@gmail.com>
2023-11-04 17:03:57 +0100

    Update score fixutures

--- src/frontend/interfaces/pitch/components/SelectPitchView/index.js
+++ src/frontend/interfaces/pitch/components/SelectPitchView/index.js
@@ -1,13 +1,26 @@
+import React, { useEffect } from "react"
 import SelectPitch from "./SelectPitch"
 
 const SelectPitchView = () => {
     let pitches = []
+    const [pitchData, setPitchData] = useState([]); // useState hook to store data
+    useEffect(() => {
+        // Define the API call here
+        async function fetchData() {
+          try {
+            google.script.run
+            .withSuccessHandler(setPitchData)
+            .getListOfPitches()
+            
+            setData(jsonData);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        }
     
-    google.script.run
-        .withSuccessHandler(data => {
-            pitches = data
-        })
-        .getListOfPitches()
+        // Call the API when the component is mounted
+        fetchData();
+      }, []);
 
     return <>
         <h2>Please select pitch</h2>
