c30af2b4eccdce5567aa0552f3f14a0f503aff12
Chris Kelly <eurochriskelly@gmail.com>
2023-11-06 08:51:23 +0100

    Display improvements in field cards
    - Second team was not showing as winner
    - Font-size increased

--- src/frontend/interfaces/pitch/generic/ScoreDisplay/index.js
+++ src/frontend/interfaces/pitch/generic/ScoreDisplay/index.js
@@ -16,15 +16,17 @@ const ScoreDisplay = ({ header, goals, points }) => {
         padding: '0.8rem 0.5rem',
         fontSize: '0.7em',
         minWidth: '100px',
-        margin: '0.8rem'
+        margin: '0.8rem 2rem',
+        userSelect: 'none',
     }
     if (!havePoints && !haveGoals) {
         return <span style={style}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
     }
     return <span style={{
-        fontSize: '2.4rem',
+        fontSize: '2.6rem',
         textAlign: 'center',
         padding: '9px 0',
+        userSelect: 'none',
         backgroundColor: 'rgba(179,198,179,0.5)',
     }}>{goals||'0'}-{points||'0'} <span style={{ color: '#888'}}>(<span style={{fontWeight: 'bold', color: '#000'}}>{
         (goals||0)*3 + (points||0)
