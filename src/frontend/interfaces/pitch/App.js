b47e18828cf9c5a434ddb29008fe8fda0fdac17f
Chris Kelly <eurochriskelly@gmail.com>
2023-11-09 00:50:57 +0100

    Remove player interface

--- src/frontend/interfaces/pitch/App.js
+++ src/frontend/interfaces/pitch/App.js
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import SelectPitchView from "./components/SelectPitchView";
 import PitchView from "./components/PitchView";
+import './site.css'
 
 function App() {
     return <>
-        <h1>Field Coordination</h1>
+        <h1>Field Coordination1</h1>
         <SelectPitchView />
     </>
 }
