5f4fc6ae500a2d26c3fd90ec448dd2efb57b73ba
Chris Kelly <eurochriskelly@gmail.com>
2023-11-14 17:46:06 +0100

    Latest updates

--- src/frontend/interfaces/groups/components/TournamentView/index.js
+++ src/frontend/interfaces/groups/components/TournamentView/index.js
@@ -11,10 +11,20 @@ const TournamentView = () => {
 
     const [groups, setGroups] = useState([])
     const [standings, setStandings] = useState([])
+    const [nextMatches, setNextMatches] = useState([])
     const [tournament, setTournament] = useState({})
 
     useEffect(() => {
         setTournament(location.state.tournament)
+        fetch(`/api/fixtures/nextup/${tournamentId}`)
+            .then(response => response.json())
+            .then(data => {
+                setNextMatches(data.data)
+            })
+            .catch(error => {
+                console.error('Error fetching next fixtures:', error)
+            })
+
         fetch(`/api/group/standings/${tournamentId}`)
             .then(response => response.json())
             .then(data => {
@@ -23,7 +33,7 @@ const TournamentView = () => {
                 console.log(data)
             })
             .catch(error => {
-                console.error('Error fetching data:', error)
+                console.error('Error fetching standings', error)
             })
     }, [])
     return (
@@ -35,17 +45,43 @@ const TournamentView = () => {
                 </h2>
                 {
                     groups
-                    .slice(0, 3)
-                    .map((group, id) => {
-                        return <>
-                            <section>
-                                <h3>{group}</h3>
-                                <div>
-                                    lo lo lo
-                                </div>
-                            </section>
-                        </>
-                    })
+                        .slice(0, 3)
+                        .map((group, id) => {
+                            console.log({nextMatches})
+                            return <>
+                                <section>
+                                    <h3>{group}</h3>
+                                    <div>
+                                        {
+                                            (nextMatches || [])
+                                                .filter((match) => match.category === group)
+                                                .slice(0, 3)
+                                                .map((match, i) => {
+                                                    const { scheduledTime, team1, team2, goals1, goals2, points1, points2, umpiringTeam } = match
+                                                    return (
+                                                        <div key={`match${i}`} className={styles.nextArea}>
+                                                            <div>
+                                                                <span>{scheduledTime}</span>
+                                                                <span>{team1}</span>
+                                                                <span>vs</span>
+                                                                <span>{team2}</span>
+                                                            </div>
+                                                            {
+                                                                (goals1 && goals2) && <div>
+                                                                    <span></span>
+                                                                    <span>{goals1}-{points1}</span>
+                                                                    <span></span>
+                                                                    <span>{goals2}-{points2}</span>
+                                                                </div>
+                                                            }
+                                                        </div>
+                                                    )
+                                                })
+                                        }
+                                    </div>
+                                </section>
+                            </>
+                        })
                 }
             </article>
             <article>
@@ -53,7 +89,7 @@ const TournamentView = () => {
                 {groups.filter(x => x.startsWith('Men')).map((group, id) => {
                     return (
                         <section key={`g${id}`}>
-                            <GroupStandings standings={standings.filter((team) => team.category === group)}   />
+                            <GroupStandings standings={standings.filter((team) => team.category === group)} />
                         </section>
                     )
                 })}
