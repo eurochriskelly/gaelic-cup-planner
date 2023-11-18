c0b0de571b46230fac065319a9df1b2ae320e634
Chris Kelly <eurochriskelly@gmail.com>
2023-11-19 00:09:21 +0100

    Switch pitch view from css to scss

--- src/frontend/interfaces/pitch/components/PitchView/Fixture/index.js
+++ src/frontend/interfaces/pitch/components/PitchView/Fixture/index.js
@@ -6,7 +6,7 @@ const Fixture = ({ fixture, isFocus }) => {
     const {
         id, tournamentId,
         scheduled = '', started = '',
-        startedTime = '',  scheduledTime ='',
+        startedTime = '', scheduledTime = '',
         category, stage,
         team1, goals1, points1,
         team2, goals2, points2,
@@ -44,30 +44,34 @@ const Fixture = ({ fixture, isFocus }) => {
         return category.replace(/[^A-Z0-9]/g, '');
     }
 
-    return <div className={`${styles.fixture} ${isFocus ? styles.focusFixture : ''}`} key={id}>
-        <div className={rowClasses()} style={{ backgroundColor: scoreUpToDate ? '#bcc6bc' : '' }}>
-            <div>
-                <ClockIcon started={started} scheduled={scheduledTime} focus={isFocus} played={scoreUpToDate} />
-                <span>
+    return (
+        <div className={`${styles.fixture} ${isFocus ? styles.focusFixture : ''}`} key={id}>
+            <div className={rowClasses()} style={{ backgroundColor: scoreUpToDate ? '#bcc6bc' : '' }}>
+                <div>
+                    <ClockIcon started={started} scheduled={scheduledTime} focus={isFocus} played={scoreUpToDate} />
                     <span>
-                        <label>STAGE:</label>{stage}</span>
-                    <span>{categoryLabel()}</span>
-                </span>
-            </div>
-            <div>
-                <div className={(winner === team1 ? styles.winner : '') + ' teamName'}>{
-                    formatName(team1)
-                }</div>
-                <ScoreDisplay goals={goals1} points={points1} />
-            </div>
-            <div>
-                <div className={(winner === team2 ? styles.winner : '') + ' teamName'}>{
-                    formatName(team2)
-                }</div>
-                <ScoreDisplay goals={goals2} points={points2} />
+                        <span>
+                            <label>STAGE:</label>
+                            {stage}
+                        </span>
+                        <span>{categoryLabel()}</span>
+                    </span>
+                </div>
+                <div>
+                    <div className={(winner === team1 ? styles.winner : '') + ' teamName'}>{
+                        formatName(team1)
+                    }</div>
+                    <ScoreDisplay goals={goals1} points={points1} />
+                </div>
+                <div>
+                    <div className={(winner === team2 ? styles.winner : '') + ' teamName'}>{
+                        formatName(team2)
+                    }</div>
+                    <ScoreDisplay goals={goals2} points={points2} />
+                </div>
             </div>
         </div>
-    </div>
+    )
 }
 
 export default Fixture;
