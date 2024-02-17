8b721ffb0f89393b6da94f5a1159203b434d65bd
Chris Kelly <eurochriskelly@gmail.com>
2024-02-18 00:02:07 +0100

    Make score selector a little more solid

--- src/frontend/interfaces/pitch/components/PitchView/UpdateFixture/DrawerFinish/ScoreSelect/ScoreSelect.stories.jsx
+++ src/frontend/interfaces/pitch/components/PitchView/UpdateFixture/DrawerFinish/ScoreSelect/ScoreSelect.stories.jsx
@@ -2,17 +2,47 @@ import ScoreSelect from './';
 import styles from './ScoreSelect.module.scss'
 
 export default {
-  title: 'pitch/Pitch/UpdateFixture/ScoreSelect',
+  title: 'pitch/UpdateFixture/DrawerFinish/ScoreSelect',
   component: ScoreSelect,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
 }
-export const BeforeMatchStarts = {
+
+export const NewScore = {
+  args: {
+    scores: {
+      Team1: { goals: null, points: null },
+      Team2: { goals: null, points: null },
+    },
+    currentTeam: 'Team1',
+    updateScore: x => {
+      console.log('New Score', x)
+    },
+  },
+};
+
+
+export const UpdateScore = {
   args: {
     scores: {
       Team1: { goals: 3, points: 12 },
+      Team2: { goals: 3, points: 12 },
+    },
+    currentTeam: 'Team1',
+    updateScore: x => {
+      console.log('New Score', x)
+    },
+  },
+};
+
+
+// FIXME: high score breaks!!
+export const UpdateHighScore = {
+  args: {
+    scores: {
+      Team1: { goals: 13, points: 42 },
       Team2: { goals: 3, points: 12 },
     },
     currentTeam: 'Team1',
