c8d0712769c819aa9d53834879773bd611213ee7
Chris Kelly <eurochriskelly@gmail.com>
2024-02-23 08:14:14 +0100

    Create tab header navigation

---
+++ src/frontend/interfaces/pitch/components/PitchView/PitchViewHeader/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import styles from "./PitchViewHeader.module.scss";
+
+const PitchViewHeader = ({ pitchId, tab = "Next", backToSelection, changeTab }) => {
+  const tabNames = ["Next", "Finished", "Unplayed"];
+  const [currentTab, setCurrentTab] = useState(tab);
+
+  const tabSelected = tab => {
+    setCurrentTab(tab)
+    changeTab(tab)
+  }
+  return (
+    <div className={styles.fixturesHead}>
+      <h2>
+        <span onClick={backToSelection}></span>
+        <span>Fixtures for pitch: {pitchId}</span>
+      </h2>
+
+      <div className={styles.navBar}>
+        {tabNames.map((tn, i) => (
+          <span
+            id={`tn${i}`}
+            onClick={tabSelected.bind(null, tn)}
+            className={`tab-${tn} ${tn === currentTab ? styles.selected : ""}`}
+          >
+            {tn}
+          </span>
+        ))}{" "}
+      </div>
+    </div>
+  );
+};
+
+export default PitchViewHeader;
