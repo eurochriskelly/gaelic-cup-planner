3ee02fbb26e7868e56997903a22eb3488c7edca4
Chris Kelly <eurochriskelly@gmail.com>
2024-02-24 13:41:55 +0100

    Added tabs for viewing fixtures of different statuses

--- src/frontend/interfaces/pitch/components/PitchView/PitchViewHeader/index.js
+++ src/frontend/interfaces/pitch/components/PitchView/PitchViewHeader/index.js
@@ -10,8 +10,8 @@ const PitchViewHeader = ({ pitchId, tab = "Next", backToSelection, changeTab }) 
     changeTab(tab)
   }
   return (
-    <div className={styles.fixturesHead}>
-      <h2>
+    <div className={styles.fixturesHead} style={{border: '2px solid red'}}>
+      <h2 >
         <span onClick={backToSelection}></span>
         <span>Fixtures for pitch: {pitchId}</span>
       </h2>
@@ -19,7 +19,7 @@ const PitchViewHeader = ({ pitchId, tab = "Next", backToSelection, changeTab }) 
       <div className={styles.navBar}>
         {tabNames.map((tn, i) => (
           <span
-            id={`tn${i}`}
+            key={`tn${i}`}
             onClick={tabSelected.bind(null, tn)}
             className={`tab-${tn} ${tn === currentTab ? styles.selected : ""}`}
           >
