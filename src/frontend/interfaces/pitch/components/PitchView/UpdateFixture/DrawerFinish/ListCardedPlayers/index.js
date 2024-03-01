be35e5f11f17d24d8da8dcc1cdfbf2499e0936c5
Chris Kelly <eurochriskelly@gmail.com>
2024-03-01 21:33:53 +0100

    Card information gatherered and passed to parent component
    - Needs to be delivered to backend

--- src/frontend/interfaces/pitch/components/PitchView/UpdateFixture/DrawerFinish/ListCardedPlayers/index.js
+++ src/frontend/interfaces/pitch/components/PitchView/UpdateFixture/DrawerFinish/ListCardedPlayers/index.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react'
-import styles from './ListCardedPlayers.module.scss'
+import React, { useState } from "react";
+import CardSelector from "./CardSelector";
+import styles from "./ListCardedPlayers.module.scss";
 
 const CardedPlayerRow = ({ id, data, onAdd, onRemove }) => {
-  const [playerNumber, setPlayerNumber] = useState('');
-  const [playerName, setPlayerName] = useState('');
+  const [playerNumber, setPlayerNumber] = useState("");
+  const [playerName, setPlayerName] = useState("");
+  const [playerCard, setPlayerCard] = useState("");
 
   const buttonStyle = () => {
-    return playerNumber && playerName ? 'enabled' : 'disabled'
-  }
-  const filled = playerName && playerNumber
+    return playerNumber && playerName ? "enabled" : "disabled";
+  };
+  const filled = playerName && playerNumber && playerCard;
   return (
-    <div key={`cardedplayer-${id}`}>
+    <div className={styles.CardedPlayerRow} key={`cardedplayer-${id}`}>
+      <CardSelector onCard={(card) => {
+        console.log('Setting card to: ', card)
+        setPlayerCard(card)     
+      }} />
       <input
         type="number"
-        placeholder="Player Number"
+        placeholder="#"
         value={playerNumber}
         onChange={(e) => setPlayerNumber(e.target.value)}
       />
@@ -23,55 +29,81 @@ const CardedPlayerRow = ({ id, data, onAdd, onRemove }) => {
         value={playerName}
         onChange={(e) => setPlayerName(e.target.value)}
       />
-      <button disabled={!filled} className={buttonStyle()} onClick={() => {
-        if (data) {
-          onRemove(data)
-        } else {
-          onAdd({ id, playerNumber, playerName })
-        }
-      }}>{data ? '-' : '+'}</button>
+      <button
+        disabled={!filled}
+        className={buttonStyle()}
+        onClick={() => {
+          const playerData= {
+            id,
+            playerCard,
+            playerNumber,
+            playerName,
+          }
+          console.log('playerData: ', playerData)
+          if (data) {
+            console.log('removing something')
+            onRemove(data);
+          } else {
+            console.log('adding something')
+            onAdd(playerData);
+          }
+        }}
+      >{data ? "-" : "+"}</button>
     </div>
-  )
-}
+  );
+};
 
-const ListCardedPlayers = () => {
-  const [allowClose, setAllowClose] = useState(false)
-  const [cardedPlayers, setCardedPlayers] = useState([])
+const ListCardedPlayers = ({
+  onProceed = () => {}
+}) => {
+  const [allowClose, setAllowClose] = useState(false);
+  const [cardedPlayers, setCardedPlayers] = useState([]);
   const actions = {
-    cardPlayersUpdated: () => {
-      console.log('cardPlayersUpdated')
-    },
     onAdd: (details) => {
+      console.log(details)
       setCardedPlayers([...cardedPlayers, details])
     },
     onRemove: (details) => {
       const newCardedPlayers = cardedPlayers.filter((player) => {
-        return player.id !== details.id
-      })
-      setCardedPlayers(newCardedPlayers)
-    }
-  }
-
+        return player.id !== details.id;
+      });
+      setCardedPlayers(newCardedPlayers);
+    },
+  };
   return (
-    <div style={styles.cardedPlayerRow}>
-      <h2>Carded players?</h2>
-      <div>
-      {
-        [...cardedPlayers, ''].map((player, index) => {
-          const data = index < cardedPlayers.length ? cardedPlayers[index] : null
+    <div className={styles.ListCardedPlayers}>
+      <div style={{ display: allowClose ? "none" : "block" }}>
+        {[...cardedPlayers, ""].map((player, index) => {
+          const data = index < cardedPlayers.length ? cardedPlayers[index] : null;
           return (
-            <CardedPlayerRow id={index} data={data} onAdd={actions.onAdd} onRemove={actions.onRemove} />
-          )
-        })  
-      }
+            <CardedPlayerRow
+              key={`cpr-${index}`}
+              id={index}
+              data={data}
+              onAdd={actions.onAdd}
+              onRemove={actions.onRemove}
+            />
+          );
+        })}
       </div>
-      <div>
-        <button onClick={actions.cardPlayersUpdated} className={allowClose ? 'enabled' : 'disabled'}>
-          Done
-        </button>
+      <div className={styles.ready}>
+        <input
+          type="checkbox"
+          id="allowClose"
+          onChange={() => console.log('is this running?') || setAllowClose(!allowClose)}
+        />
+        <label>Ready to proceed?</label>
       </div>
+      <button
+        onClick={onProceed.bind(null, cardedPlayers)}
+        className="enabled"
+        style={{ display: allowClose ? "block" : "none" }}
+      >
+        Proceed to next game!
+      </button>
     </div>
-  )
+  );
 };
 
 export default ListCardedPlayers;
+
