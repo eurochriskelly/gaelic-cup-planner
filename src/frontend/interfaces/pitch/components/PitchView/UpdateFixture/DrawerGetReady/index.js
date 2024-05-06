ce1d9d2ac9345130435689b4a1e8f2c6eef64e6c
Chris Kelly <eurochriskelly@gmail.com>
2024-05-07 00:25:20 +0200

    Update the format of points to x-xx (xx)

--- src/frontend/interfaces/pitch/components/PitchView/UpdateFixture/DrawerGetReady/index.js
+++ src/frontend/interfaces/pitch/components/PitchView/UpdateFixture/DrawerGetReady/index.js
@@ -1,89 +1,91 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const DrawerGetReady = ({
-    fixture,
-    visible,
-    startMatch,
-    onClose = () => { }
+  fixture,
+  visible,
+  startMatch,
+  onClose = () => {},
 }) => {
-    if (!visible) return null;
+  if (!visible) return null;
 
-    const { umpireTeam } = fixture;
+  const { umpireTeam } = fixture;
 
-    // Initialize state for checkboxes
-    const [checklist, setChecklist] = useState({
-        pitchMarked: false,
-        sheetsSubmitted: false,
-        umpiresReady: false,
-    });
+  // Initialize state for checkboxes
+  const [checklist, setChecklist] = useState({
+    pitchMarked: false,
+    sheetsSubmitted: false,
+    umpiresReady: false,
+  });
 
-    // Function to handle checkbox change
-    const handleCheckboxChange = (event) => {
-        const { name, checked } = event.target;
-        setChecklist(prevChecklist => ({
-            ...prevChecklist,
-            [name]: checked,
-        }));
-    };
+  // Function to handle checkbox change
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    setChecklist((prevChecklist) => ({
+      ...prevChecklist,
+      [name]: checked,
+    }));
+  };
 
-    // Check if all checkboxes are checked
-    const allChecked = Object.values(checklist).every(value => value);
+  // Check if all checkboxes are checked
+  const allChecked = Object.values(checklist).every((value) => value);
 
-    const actions = {
-        startMatch: async () => {
-            await startMatch(fixture.id);
-            onClose('start');
-        },
-    };
+  const actions = {
+    startMatch: async () => {
+      await startMatch(fixture.id);
+      onClose("start");
+    },
+  };
 
-    return (
-        <div className='drawerGetReady'>
-            <div className='drawer-header'>Pre-match checklist</div>
-            <div className='drawer-container'>
-                <div>
-                    <ul>
-                        <li>
-                            <input
-                                className="bigcheck"
-                                type="checkbox"
-                                name="pitchMarked"
-                                checked={checklist.pitchMarked}
-                                onChange={handleCheckboxChange}
-                            />
-                            <span>Is pitch marked out?</span>
-                        </li>
-                        <li>
-                            <input
-                                className='bigcheck'
-                                type="checkbox"
-                                name="sheetsSubmitted"
-                                checked={checklist.sheetsSubmitted}
-                                onChange={handleCheckboxChange}
-                            />
-                            <span>Have both teams submitted sheets?</span>
-                        </li>
-                        <li>
-                            <input
-                                className='bigcheck'
-                                type="checkbox"
-                                name="umpiresReady"
-                                checked={checklist.umpiresReady}
-                                onChange={handleCheckboxChange}
-                            />
-                            <span>Are <span>{umpireTeam}</span> umpires ready?</span>
-                        </li>
-                    </ul>
-                    <button
-                        className={allChecked ? "enabled" : "disabled"}
-                        onClick={actions.startMatch}
-                        disabled={!allChecked}
-                    >
-                        Start the clock ...
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="drawerGetReady">
+      <div className="drawer-header">Pre-match checklist</div>
+      <div className="drawer-container">
+        <div>
+          <ul>
+            <li>
+              <input
+                className="bigcheck"
+                type="checkbox"
+                name="pitchMarked"
+                checked={checklist.pitchMarked}
+                onChange={handleCheckboxChange}
+              />
+              <span>Is pitch marked out?</span>
+            </li>
+            <li>
+              <input
+                className="bigcheck"
+                type="checkbox"
+                name="sheetsSubmitted"
+                checked={checklist.sheetsSubmitted}
+                onChange={handleCheckboxChange}
+              />
+              <span>Have both teams submitted sheets?</span>
+            </li>
+            <li>
+              <input
+                className="bigcheck"
+                type="checkbox"
+                name="umpiresReady"
+                checked={checklist.umpiresReady}
+                onChange={handleCheckboxChange}
+              />
+              <span>
+                Are <span>{umpireTeam}</span> umpires ready?
+              </span>
+            </li>
+          </ul>
+          <button
+            className={allChecked ? "enabled" : "disabled"}
+            onClick={actions.startMatch}
+            disabled={!allChecked}
+          >
+            Start the clock ...
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DrawerGetReady;
