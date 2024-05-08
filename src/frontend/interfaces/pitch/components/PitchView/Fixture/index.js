39f2b2e5d4b4a4154b41f828827363275a7b72fa
Chris Kelly <eurochriskelly@gmail.com>
2024-05-08 12:20:41 +0200

    The clock should show delays in minutes in brackets

--- src/frontend/interfaces/pitch/components/PitchView/Fixture/index.js
+++ src/frontend/interfaces/pitch/components/PitchView/Fixture/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { formatTeamName } from "../../common/TeamNameDisplay";
+import { formatTeamName, militaryTimeDiffMins } from "../../common/TeamNameDisplay";
 import ScoreDisplay from "../../../../../shared/generic/ScoreDisplay";
 import ClockIcon from "../../../../../shared/generic/ClockIcon";
 
@@ -27,15 +27,13 @@ const Fixture = ({ fixture, isFocus }) => {
   const totalTeam2 = (goals2 || 0) * 3 + (points2 || 0);
   let winner = null;
   if (played) {
-    totalTeam1 > totalTeam2 ? team1 : totalTeam1 < totalTeam2 ? team2 : "";
+    winner = totalTeam1 > totalTeam2 ? team1 : totalTeam1 < totalTeam2 ? team2 : "";
   }
 
   const scoreUpToDate = !!played;
   const rowClasses = () => {
     const classes = [];
-    if (scoreUpToDate) {
-      classes.push("scoreUpToDate");
-    }
+    if (scoreUpToDate) classes.push("scoreUpToDate");
     return classes.join(" ");
   };
 
@@ -49,50 +47,23 @@ const Fixture = ({ fixture, isFocus }) => {
     );
   };
 
-  const categoryLabel = () => {
-    return group?.replace(/[^A-Z0-9]/g, "");
-  };
-
+ console.log('sssss', scheduledTime, startedTime)
   return (
-    <div
-      className={`fixture ${isFocus ? 'focusFixture' : ""}`}
-      key={id}
-    >
+    <div className={`fixture ${isFocus ? 'focusFixture' : ""}`} key={id}>
       <div
         className={rowClasses()}
-        style={{ backgroundColor: scoreUpToDate ? "#bcc6bc" : "" }}
-      >
-        <div>
-          <ClockIcon
-            started={startedTime}
-            scheduled={scheduledTime}
-            focus={isFocus}
-            played={scoreUpToDate}
-          />
-          <span>
-            <span>
-              <label>STAGE:</label>
-              {stage}
-            </span>
-            <span>{categoryLabel()}</span>
-          </span>
-        </div>
-        <div>
-          <div
-            className={(winner === team1 ? 'winner' : "") + " teamName"}
-          >
-            {formatName(team1, winner === team1)}
-          </div>
-          <ScoreDisplay goals={goals1} points={points1} played={played} />
-        </div>
-        <div>
-          <div
-            className={(winner === team2 ? 'winner' : "") + " teamName"}
-          >
-            {formatName(team2, winner === team2)}
-          </div>
-          <ScoreDisplay goals={goals2} points={points2} played={played} />
-        </div>
+        style={{ backgroundColor: scoreUpToDate ? "#bcc6bc" : "" }}>
+        <ClockDisplay
+          scheduledTime={scheduledTime}
+          startedTime={startedTime}
+          delayMinutes={militaryTimeDiffMins(scheduledTime, startedTime)}
+          focus={focus}
+          scoreUpToDate={scoreUpToDate}
+          stage={stage}
+          group={group}
+        />
+        <TeamResult goals={goals1} points={points1} team={team1} winner={winner} played={played} />
+        <TeamResult goals={goals2} points={points2} team={team2} winner={winner} played={played} />
         {!played && (
           <div className='umpires'>
             {umpireTeam ? (
@@ -111,3 +82,50 @@ const Fixture = ({ fixture, isFocus }) => {
 };
 
 export default Fixture;
+
+function TeamResult({
+  goals, points, played, team, winner
+}) {
+  console.log(winner, team)
+  const isWinner = winner === team
+  return (
+    <div>
+      <div className={(isWinner ? 'winner' : 'loser') + " teamName"}>
+        {formatTeamName(team, isWinner)}
+      </div>
+      <ScoreDisplay goals={goals} points={points} played={played} />
+    </div>
+  )
+}
+
+function ClockDisplay({
+  scheduledTime,
+  startedTime,
+  delayMinutes = 0,
+  focus,
+  scoreUpToDate,
+  stage,
+  group
+}) {
+  const categoryLabel = () => {
+    return group?.replace(/[^A-Z0-9]/g, "");
+  };
+  return (
+    <div className="ClockDisplay">
+      <ClockIcon
+        scheduled={scheduledTime}
+        started={startedTime}
+        delay={delayMinutes}
+        focus={focus}
+        played={scoreUpToDate}
+      />
+      <span>
+        <span>
+          <label>STAGE:</label>
+          {stage}
+        </span>
+        <span>{categoryLabel()}</span>
+      </span>
+    </div>
+  )
+}
