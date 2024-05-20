7297eb6afe87e6b114122ba066f30ea0a62a65c3
Chris Kelly <eurochriskelly@gmail.com>
2024-05-20 23:04:11 +0200

    Group standings restyled to support user view and/or desktop view

--- src/frontend/interfaces/groups/components/TournamentView/GroupStandings/index.js
+++ src/frontend/interfaces/groups/components/TournamentView/GroupStandings/index.js
@@ -9,7 +9,7 @@ const GroupStandings = ({ group, standings }) => {
       return (
         <tr key={team.id}>
           <td colSpan="9" className='groupHeader'>
-            {team.category} / Group {team.grp}
+            Group {team.grp}
           </td>
         </tr>
       );
@@ -32,53 +32,55 @@ const GroupStandings = ({ group, standings }) => {
           </tr>
         </thead>
         <tbody>
-          {standings.map((team, si) => {
-            const {
-              id,
-              MatchesPlayed,
-              MatchesPlanned,
-              Wins,
-              Draws,
-              Losses,
-              PointsFrom,
-              PointsDifference,
-              TotalPoints,
-            } = team;
-            const mp = MatchesPlayed;
-            const finished = mp === MatchesPlanned;
-            return (
-              <>
-                {lastGroup !== team.group && <GroupDivider key={`gd-${si}`} team={team} />}
-                <tr key={`standing-row-${id}`}>
-                  <td
-                    className='teamName'
-                    style={{
-                      fontWeight: finished ? "bold" : "normal",
-                      color: finished ? "black" : "grey",
-                    }}
-                  >
-                    {team.team}
-                  </td>
-                  <td>
-                    {MatchesPlayed}
-                    <span className='lessRelevant'>/{MatchesPlanned}</span>
-                  </td>
-                  <td>{mp ? Wins : <NoData />}</td>
-                  <td>{mp ? Draws : <NoData />}</td>
-                  <td>{mp ? Losses : <NoData />}</td>
-                  <td>{mp ? PointsFrom : <NoData />}</td>
-                  <td>{mp ? PointsDifference : <NoData />}</td>
-                  <td>
-                    {mp ? (
-                      <div className='total'>{TotalPoints}</div>
-                    ) : (
-                      <NoData />
-                    )}
-                  </td>
-                </tr>
-              </>
-            );
-          })}
+          {standings
+            .filter((team, i) => {
+              if (i === 0) console.log('first team', team)
+              return true
+            })
+            .filter((team) => team.category === group)
+            .map((team, si) => {
+              const {
+                MatchesPlayed,
+                MatchesPlanned,
+                Wins,
+                Draws,
+                Losses,
+                PointsFrom,
+                PointsDifference,
+                TotalPoints,
+              } = team;
+              console.log(MatchesPlayed, group)
+              const mp = MatchesPlayed;
+              const finished = mp === MatchesPlanned;
+              return (
+                <>
+                  {lastGroup !== team.group && <GroupDivider key={`gd-${si}`} team={team} />}
+                  <tr key={`standing-row-${si}`}>
+                    <td className='teamName'
+                      style={{
+                        fontWeight: finished ? "bold" : "normal",
+                        color: finished ? "black" : "grey",
+                      }}>{team.team}</td>
+                    <td>
+                      <span>{MatchesPlayed}</span>
+                      <span className='lessRelevant'>/{MatchesPlanned}</span>
+                    </td>
+                    <td>{mp ? Wins : <NoData />}</td>
+                    <td>{mp ? Draws : <NoData />}</td>
+                    <td>{mp ? Losses : <NoData />}</td>
+                    <td>{mp ? PointsFrom : <NoData />}</td>
+                    <td>{mp ? PointsDifference : <NoData />}</td>
+                    <td>
+                      {mp ? (
+                        <div className='total'>{TotalPoints}</div>
+                      ) : (
+                        <NoData />
+                      )}
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
         </tbody>
       </table>
     </>
