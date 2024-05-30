b425dafef427b67d4b913e27285acd5faa0d3399
Chris Kelly <eurochriskelly@gmail.com>
2024-05-30 15:47:40 +0200

    Refactor group interface to use Mobile components

--- src/frontend/interfaces/groups/components/SelectTournamentView/index.js
+++ src/frontend/interfaces/groups/components/SelectTournamentView/index.js
@@ -1,81 +1,79 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import MobileInterface from "../../../../shared/generic/MobileInterface";
+import MobileSelect from "../../../../shared/generic/MobileSelect";
 import MainCard from "../../../../shared/generic/MainCard";
 import { sections } from "../../../../../../config/config";
 
+const tournamentId = 9;
+
 const SelectTournamentView = () => {
-  console.log("ok wonet");
   const navigate = useNavigate();
-  const [tournaments, setTournaments] = useState([]);
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
-    fetch("/api/tournaments")
+    fetch(`/api/tournaments/${tournamentId}/categories`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setTournaments(data.data);
+        setCategories(data.data);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
 
-  const onSelect = (tournamentId) => {
-    console.log("Selected tournament", tournamentId);
-    const tournament = tournaments.find((t) => t.id === tournamentId);
-    console.log("Selected tournament", tournament);
-    navigate(`/tournaments/${tournamentId}`, { state: { tournament } });
+  const handle = {
+    select: (cat) => {
+      console.log("Selected category is ", cat);
+      const category = categories.find((t) => t.category === cat);
+      navigate(`/tournament/${tournamentId}/category/${cat}`, { state: { category} });
+    },
+    back: () => {},
   };
 
   return (
-    <MobileInterface
-      title="Live tournament updates"
-      topTitle="Select competition"
-      tabNames={["upcoming", "standings", "knockouts"]}
-      // FIXME: only pass selected card
-      cardInfo={cardInfo}
-      viewType="topview">
-      <IfTournamentSelected />
-      <IfShowCards tournaments={tournaments} />
+    <MobileInterface>
       <MobileSelect sections={sections}>
+        <div>Select competition</div>
+        {categories.map(
+          ({ category, stage, totalGames, currentGame, brackets }) => (
+            <MainCard id={category} heading={`${category}`} onSelect={handle.select}>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Stage</td>
+                    <td>{stage}</td>
+                  </tr>
+                  <tr>
+                    <td>Game #</td>
+                    <td>{currentGame}</td>
+                  </tr>
+                  <tr>
+                    <td>Total matches</td>
+                    <td>{totalGames}</td>
+                  </tr>
+                  <tr>
+                    <td>Brackets</td>
+                    <td>
+                      <div className="pills">
+                        {brackets.map((b, i) => {
+                          return (
+                            <span key={`bracket-${i}`} className="pill">
+                              {b}
+                            </span>
+                          );
+                        })}
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </MainCard>
+          )
+        )}
+      </MobileSelect>
     </MobileInterface>
   );
 };
 
 export default SelectTournamentView;
-
-// IMPL
-
-function IfTournamentSelected() {
-  return <div>
-    <div> foo bar</div>
-    <div>fee boar</div>
-    <div>fwee burr</div>
-  </div>
-}
-
-function IfShowCards ({
-  tournaments = []
-}) {
-  return <div>
-    {
-      cardInfo.map(card => {
-        
-      })
-    }
-    {tournaments.map(({ Id, Title, Date, Tournament }) => (
-      <MainCard id={Id} heading={Title}>
-        <div>
-          <div>
-            <span>Date</span>
-            <span>{Date?.substring(0, 10)}</span>
-          </div>
-          <div>
-            <span>Location</span>
-            <span>{Location}</span>
-          </div>
-        </div>
-      </MainCard>
-    ))}
-  </div> 
-}
