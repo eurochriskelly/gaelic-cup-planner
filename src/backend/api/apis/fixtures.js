e7d429a290eac8b99e6fb9e6a3abaf7af227facf
Chris Kelly <eurochriskelly@gmail.com>
2024-06-02 18:36:48 +0200

    Refactor api so all routes are on the entry page

---
+++ src/backend/api/apis/fixtures.js
@@ -0,0 +1,237 @@
+const { II, DD, EE } = require("../../lib/logging");
+
+module.exports = (app, db, select) => {
+  return {
+    /* Check if a given stage is complete */
+
+    /* Update the score
+     * 1. Update the score
+     * 2. Check if the stage is complete
+     *    2.1 If yes, update the playoff fixtures based on new information
+     */
+    updateScore: async (req, res) => {
+      const { id, tournamentId } = req.params;
+      II(
+        "Calling API: /api/tournaments/" +
+          tournamentId +
+          "fixtures/" +
+          id +
+          "/score"
+      );
+      try {
+        console.log("Body contains the following");
+        console.log(req.body);
+        const { team1, team2 } = req.body;
+        const team1Score = team1.goals * 3 + team1.points;
+        const team2Score = team2.goals * 3 + team2.points;
+        const updateQuery = [
+          "UPDATE fixtures",
+          `SET goals1 = '${team1.goals}', points1 = '${team1.points}', `,
+          `    goals2 = '${team2.goals}', points2 = '${team2.points}'`,
+          `WHERE id = ${id};`,
+        ].join(" ");
+        console.log("score update", updateQuery);
+        await select(updateQuery);
+        // TODO:: These queries can be run in parallel or in a combined statement
+        await processStageCompletion(id);
+        await processAnyMatchDependentFixtures({
+          name: team1.name,
+          category: team1.category,
+          position: team1Score > team2Score ? 1 : 2,
+          matchId: id,
+          tournamentId,
+        });
+        await processAnyMatchDependentFixtures({
+          name: team2.name,
+          category: team2.category,
+          position: team1Score > team2Score ? 2 : 1,
+          matchId: id,
+          tournamentId,
+        });
+        res.json({ message: "Score updated successfully." });
+      } catch (err) {
+        console.log("Error occured", err);
+        return res.status(500).json({ error: err.message });
+      }
+    },
+
+    cardPlayers: (req, res) => {
+      const { id, tournamentId } = req.params;
+      II(`Calling API: /api/tournaments/${tournamentId}/fixtures/${id}/carded`);
+      const query = [
+        "INSERT INTO cards",
+        "(tournament, fixture, playerNumber, playerName, cardColor, team)",
+        "VALUES",
+        req.body
+          .map((player) => {
+            return `(${tournamentId}, ${id}, ${player.playerNumber}, '${player.playerName}', '${player.playerCard}', '${player.team}')`;
+          })
+          .join(","),
+      ].join(" ");
+      db.query(query, (err, results) => {
+        if (err) {
+          console.log("Error occured", err);
+          return res.status(500).json({ error: err.message });
+        }
+        res.json({ data: results });
+      });
+    },
+
+    startFixture: (req, res) => {
+      const { id, tournamentId } = req.params;
+      II(`Calling API: /api/tournaments/${tournamentId}/fixtures/${id}/start`);
+      const mysqlTimestamp = new Date()
+        .toISOString()
+        .slice(0, 19)
+        .replace("T", " ");
+      const query = [
+        "UPDATE fixtures",
+        `SET started = '${mysqlTimestamp}'`,
+        `WHERE id = ${id};`,
+      ].join(" ");
+
+      db.query(query, (err, results) => {
+        if (err) {
+          console.log("Error occured", err);
+          return res.status(500).json({ error: err.message });
+        }
+        res.json({ data: results });
+      });
+    },
+
+    nextFixtures: (req, res) => {
+      const { tournamentId } = req.params;
+      II(`Calling API: /api/tournaments/${tournamentId}/fixtures/nextup`);
+      const query = `SELECT * FROM v_next_up WHERE tournamentId = ${tournamentId}`;
+      db.query(query, (err, results) => {
+        if (err) {
+          console.log("Error occured", err);
+          return res.status(500).json({ error: err.message });
+        }
+        res.json({ data: results });
+      });
+    },
+
+    fixturesByPitch: (req, res) => {
+      const { pitch, tournamentId } = req.params;
+      II(`Calling API: /api/tournaments/${tournamentId}/fixtures/:pitch`);
+      let where = pitch ? `WHERE pitch = '${pitch}'` : "";
+      const query = `SELECT * FROM v_fixture_information ${where}`;
+      db.query(query, (err, results) => {
+        if (err) {
+          return res.status(500).json({ error: err.message });
+        }
+        res.json({ data: results });
+      });
+    },
+  };
+};
+
+async function processAnyMatchDependentFixtures(teamInfo) {
+  // Any time a match is updated attempt to update any teams were dependent on the outcome
+  const { name, position, matchId, category, tournamentId } = teamInfo;
+  const placeHolder = `~match:${matchId}/p:${position}`;
+  const qAnyMatchWinner = (x) =>
+    [
+      `UPDATE fixtures `,
+      `SET ${x}Id = '${name}' `,
+      `WHERE `,
+      `  ${x}Planned = '${placeHolder}' and `,
+      `  tournamentId = ${tournamentId} and `,
+      `  category = '${category}'`,
+    ].join(" ");
+  console.log(
+    "Updating fixtures depending out match outcome for match [",
+    matchId,
+    "]"
+  );
+  console.log(qAnyMatchWinner("team1"));
+  console.log(qAnyMatchWinner("team2"));
+  console.log(qAnyMatchWinner("umpireTeam"));
+  await select(qAnyMatchWinner("team1"));
+  await select(qAnyMatchWinner("team2"));
+  await select(qAnyMatchWinner("umpireTeam"));
+}
+
+async function processStageCompletion(fixtureId) {
+  const selQuery = [
+    `SELECT tournamentId, stage, groupNumber, `,
+    `  category FROM fixtures WHERE id = ${fixtureId}`,
+  ].join(" ");
+  const data = await select(selQuery);
+  const { tournamentId, stage, groupNumber, category } = data?.data[0];
+  const completedQuery = [
+    `SELECT count(*) as remaining FROM fixtures WHERE `,
+    `  tournamentId = ${tournamentId} and `,
+    `  stage = '${stage}' and `,
+    `  groupNumber = ${groupNumber} and `,
+    `  category = '${category}' and `,
+    `  goals1 is null`,
+  ].join(" ");
+  const remainingMatchesInStage = +(await select(completedQuery))?.data[0]
+    ?.remaining;
+  if (!remainingMatchesInStage) {
+    const qGroupStandings = [
+      `SELECT * FROM v_group_standings where `,
+      `  tournamentId = ${tournamentId} and `,
+      `  grp = ${groupNumber} and `,
+      `  category = '${category}'`,
+    ].join(" ");
+    const groupStandings = (await select(qGroupStandings)).data;
+    // Loop over each team in stage/groupNumber and update any playoff fixtures
+    // that are dependent on this outcome of this stage
+    const qNumPositions = [
+      `SELECT count(*) as numPositions FROM v_fixture_information WHERE `,
+      `  tournamentId = ${tournamentId} and `,
+      `  stage = '${stage}' and `,
+      `  groupNumber = ${groupNumber} and `,
+      `  category = '${category}'`,
+    ].join(" ");
+    const { numPositions = 0 } =
+      stage === "group"
+        ? (await select(qNumPositions)).data[0] || {}
+        : { numPositions: 2 };
+    const range = [...Array(numPositions).keys()];
+    range.forEach(async (position) => {
+      const placeHolder = `~${stage}:${groupNumber}/p:${position + 1}`;
+      const newValue = groupStandings[position]?.team;
+      const qUpdatePlayoffTeam1 = [
+        `UPDATE fixtures `,
+        `SET team1Id = '${newValue}' `,
+        `WHERE `,
+        ` team1Planned = '${placeHolder}' and `,
+        ` tournamentId = ${tournamentId} and `,
+        ` category = '${category}'`,
+      ].join(" ");
+      const qUpdatePlayoffTeam2 = [
+        `UPDATE fixtures `,
+        `SET team2Id = '${newValue}' `,
+        `WHERE `,
+        ` team2Planned = '${placeHolder}' and `,
+        ` tournamentId = ${tournamentId} and `,
+        ` category = '${category}'`,
+      ].join(" ");
+      const qUpdateUmpireTeam = [
+        `UPDATE fixtures `,
+        `SET umpireTeamId = '${newValue}' `,
+        `WHERE `,
+        ` umpireTeamPlanned = '${placeHolder}' and `,
+        ` tournamentId = ${tournamentId} and `,
+        ` category = '${category}'`,
+      ].join(" ");
+      console.log("Updating playoff fixtures");
+      console.log(qUpdatePlayoffTeam1);
+      console.log(qUpdatePlayoffTeam2);
+      console.log(qUpdateUmpireTeam);
+      await select(qUpdatePlayoffTeam1);
+      await select(qUpdatePlayoffTeam2);
+      await select(qUpdateUmpireTeam);
+    });
+    return true;
+  }
+
+  console.log(
+    `Stage [${stage}/${groupNumber}] for [${category}] has [${remainingMatchesInStage}] remaining matches.`
+  );
+  return false;
+}
---
+++ src/backend/api/apis/tournaments.js
@@ -0,0 +1,91 @@
+const e = require("express");
+const { II, DD, EE } = require("../../lib/logging");
+
+module.exports = (app, db, select) => {
+  return {
+    getTournament: async (req, res) => {
+      const { tournamentId } = req.params;
+      II(`Calling API: /api/tournaments/${tournamentId} ...`);
+      let query = "",
+        results = [];
+      query = `SELECT * FROM tournaments WHERE id = ${tournamentId}`;
+      results = await select(query);
+      if (!results.data.length) {
+        return res.json({ data: null });
+      }
+      const tournament = results.data[0];
+      query = `SELECT category, grp, team FROM v_group_standings WHERE tournamentId = ${tournamentId}`;
+      results = await select(query);
+      tournament.groups = results.data;
+
+      query = `select id, pitch, location FROM pitches WHERE tournamentId = ${tournamentId}`;
+      results = await select(query);
+      tournament.pitches = results.data;
+
+      //	console.log(JSON.stringify(tournament, null, 2))
+      res.json({ data: tournament });
+    },
+
+    getTournaments: (req, res) => {
+      II("Calling API: /api/tournaments ...");
+      const query = `SELECT Id, Date, Title, Loctation FROM tournaments`;
+      db.query(query, (err, results) => {
+        if (err) {
+          console.log("Error occured", err);
+          return res.status(500).json({ error: err.message });
+        }
+        res.json({ data: results });
+      });
+    },
+
+    getGroups: (req, res) => {
+      const { tournamentId } = req.params;
+      II(`Calling API: /api/tournaments/${tournamentId}/groups ...`);
+      const query = `SELECT DISTINCT category FROM fixtures where tournamentId = ${tournamentId}`;
+      db.query(query, (err, results) => {
+        if (err) {
+          console.log("Error occured while getting groups", err);
+          return res.status(500).json({ error: err.message });
+        }
+        res.json({ data: results });
+      });
+    },
+
+    getCategories: (req, res) => {
+      const { tournamentId } = req.params;
+      II(`Calling API: /api/tournaments/${tournamentId}/categories ...`);
+      const query = `SELECT * FROM v_categories WHERE tournamentId = '${tournamentId}'`;
+      db.query(query, (err, results) => {
+        if (err) {
+          console.log("Error occured while getting groups", err);
+          return res.status(500).json({ error: err.message });
+        }
+        res.json({
+          data: results.map((result) => ({
+            ...result,
+            brackets: result.brackets.split(",").map((x) => x.trim()),
+          })),
+        });
+      });
+    },
+
+    getTeams: (req, res) => {
+      II(
+        "Calling API: /api/tournaments/:tournamentId/group/:group_id/teams ..."
+      );
+      const { tournamentId, group_id } = req.params;
+      const g = group_id.replace(/_/g, " ");
+      const query = `
+            SELECT id, teamName, category, groupLetter from teams where tournamentId = ${tournamentId} and category = '${g}'
+        `;
+      db.query(query, (err, results) => {
+        if (err) {
+          console.log("Error occured while getting teams", err);
+          return res.status(500).json({ error: err.message });
+        }
+        console.log(results);
+        res.json({ data: results });
+      });
+    },
+  };
+};
