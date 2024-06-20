46df89c8b25c12ba453d615eebc4857cdc1313e0
Chris Kelly <eurochriskelly@gmail.com>
2024-06-20 16:52:09 +0200

    Intoducing primereact to project

--- src/frontend/interfaces/desktop/App.jsx
+++ src/frontend/interfaces/desktop/App.jsx
@@ -1,15 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import { Provider} from "../../shared/js/Provider";
 import LandingPage from "./components/LandingPage";
+import { PrimeReactProvider } from 'primereact/api';
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 function App() {
   return (
-    <Provider>
-      <Routes>
-        <Route path="/tournament/:tournamentId" element={<LandingPage />} />
-        <Route path="*" element={<LandingPage />} />
-      </Routes>
-    </Provider>
+    <PrimeReactProvider>
+      <Provider>
+        <Routes>
+          <Route path="/tournament/:tournamentId" element={<LandingPage />} />
+          <Route path="*" element={<LandingPage />} />
+        </Routes>
+      </Provider>
+    </PrimeReactProvider>
   );
 }
 
--- src/frontend/interfaces/desktop/components/LandingPage/TournamentInfo/index.jsx
+++ src/frontend/interfaces/desktop/components/LandingPage/TournamentInfo/index.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { Calendar } from 'primereact/calendar';
 
 export default TournamentInfo;
 
@@ -6,24 +7,27 @@ function TournamentInfo({
   tournamentId 
 }) {
   const [tournInfo, setTournInfo] = useState({});
+  const [date, setDate] = useState(null);
   const base = `/tournament/${tournamentId}`;
 
   useEffect(() => {
-    fetch(base)
+    fetch(`/api/tournaments/${tournamentId}`)
       .then((response) => response.json())
       .then((data) => setTournInfo(data.data))
       .catch((error) => console.error("Error fetching tournament info:", error));
   }, [tournamentId]);
   return (
     <>
-        <h1 onClick={() => console.log(tournInfo)}>Pitch perfect desktop version</h1>
-        <table>
-          <tbody>
-            <Row label="date">{tournInfo.Date?.substring(0, 10)}</Row>
-            <Row label="title">{tournInfo.Title}</Row>
-            <Row label="location">{tournInfo.Location}</Row>
-          </tbody>
-        </table>
+      <h1 onClick={() => console.log(tournInfo)}>Pitch perfect</h1>
+      <table>
+        <tbody>
+          <Row label="date">
+              <Calendar value={tournInfo.Date?.substring(0, 10)} dateFormat='yy-mm-dd' onChange={(e) => setDate(e.value)} />
+          </Row>
+          <Row label="title">{tournInfo.Title}</Row>
+          <Row label="location">{tournInfo.Location}</Row>
+        </tbody>
+      </table>
     </>
   );
 }
