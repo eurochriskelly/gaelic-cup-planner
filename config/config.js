a0d622b332fc230c1dfe35e32731c0415b073a48
Chris Kelly <eurochriskelly@gmail.com>
2024-06-21 23:49:02 +0200

    re-instate configuration but based on environment

--- config/config.js
+++ config/config.js
@@ -1,4 +1,12 @@
+const { write } = require("xlsx");
+
 module.exports = {
+  dbConf: {
+    host: process.env['GG_HST'],
+    user: process.env['GG_USR'],
+    password: process.env['GG_PWD'],
+    database: process.env['GG_DBN']
+  },
   sections: [
     {
       title: "live competitation status",
