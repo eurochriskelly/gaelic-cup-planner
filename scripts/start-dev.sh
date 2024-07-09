671b225cb6a88558885f47a384921ad4130f7ace
Chris Kelly <eurochriskelly@gmail.com>
2024-07-09 23:30:04 +0200

    Add tailwind css to the project

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -2,7 +2,7 @@
 #
 #
 npm install
-npm install primeicons
+npm install -D tailwindcss postcss autoprefixer
 nodemon src/backend/server.js --port="4000" --app="mobile" --use-mock=true &
 vite --config vite.config-mobile.js --host &
 nodemon src/backend/server.js --port="4001" --app="desktop" --use-mock=true &
