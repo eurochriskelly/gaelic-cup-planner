b055ca6c0d889d5f6de3eb6ae2752f3651e7e7ed
Chris Kelly <eurochriskelly@gmail.com>
2024-09-12 23:49:16 +0200

    remove dropdowns from body
    should only appear when clicked on

--- src/frontend/interfaces/desktop/components/LandingPage/CompetitionsTabView/BigView/FixtureTable/index.jsx
+++ src/frontend/interfaces/desktop/components/LandingPage/CompetitionsTabView/BigView/FixtureTable/index.jsx
@@ -1,6 +1,7 @@
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
 
 function FixtureTable({ 
   fixtures,
@@ -21,45 +22,74 @@ function FixtureTable({
       <Dropdown
         value={rowData[name]}
         options={options}
-        onChange={(e) => {}}
+        onChange={(e) => {
+          console.log('this is it')
+        }}
       />
     );
   }
 
+  const textEditor = (options) => {
+    return <InputText
+      type="text" value={options.value}
+      onChange={(e) => options.editorCallback(e.target.value)}
+      onKeyDown={(e) => e.stopPropagation()}
+    />;
+  };
+
   const columns = [
     { field: "id", header: "M#", style: { width: '30px', maxWidth: '40px' }, body: (data) => `${data.id}`.substring(2) },
     { field: "scheduledTime", header: "Time", style: { width: '50px' } },
-    { field: "pitch", header: "Pitch", style: { width: '90px' } },
+    { field: "pitch", header: "Pitch", style: { width: '110px', minWidth: '90px' } },
     { field: "stage", header: "Stage", style: { width: '120px' } },
     { field: "groupNumber", header: "#", style: { width: '20px' } },
-    { field: "team1", header: "Team 1", style: { width: '150px' }, body: teamBody.bind(null, 'team1', teams) },
+    { field: "team1", header: "Team 1", style: { width: '150px' },
+      // body: teamBody.bind(null, 'team1', teams) 
+    },
     { field: "goals1", header: "G", style: { width: '20px' } },
     { field: "points1", header: "P", style: { width: '30px' }, body: (data) => `0${data.points1}`.substr(-2) },
     { field: "score1", header: "T", style: { width: '30px' }, body: (data) => `${data.points1 + (3 * data.goals1)}` },
-    { field: "team2", header: "Team 2", style: { width: '150px' }, body: teamBody.bind(null, 'team2', teams) },
+    { field: "team2", header: "Team 2", style: { width: '150px' }, 
+      // body: teamBody.bind(null, 'team2', teams)
+    },
     { field: "goals2", header: "G", style: { width: '20px' } },
     { field: "points2", header: "P", style: { width: '30px' }, body: (data) => `0${data.points2}`.substr(-2) },
     { field: "score2", header: "T", style: { width: '30px' }, body: (data) => `${data.points2 + (3 * data.goals2)}` },
-    { field: "umpireTeam", header: "Umpiring Team", style: { width: '150px' }, body: teamBody.bind(null, 'umpireTeam', umpires) },
-    { field: "referee", header: "Referee", style: { width: '150px' }, body: teamBody.bind(null, 'referee', referees) },
-  ];
+    { field: "umpireTeam", header: "Umpiring Team", style: { width: '150px' }, 
+       // body: teamBody.bind(null, 'umpireTeam', umpires)
+    },
+    { field: "referee", header: "Referee", style: { width: '150px' }, 
+      // body: teamBody.bind(null, 'referee', referees)
+    },
+  ].map(col => {
+    return {
+      ...col,
+      fnEditComplete: (e) => {
+        console.log('edit complete', e)
+      },
+      fnEditor: (options) => textEditor(options)
+    }
+  })
 
   // Filter out columns that are in the removeFields list
+
   const filteredColumns = columns.filter(col => !removeFields.includes(col.field));
 
-  return (
+  return <section className="FixtureTable">
     <DataTable value={fixtures} responsiveLayout="scroll">
       {filteredColumns.map((col, index) => (
         <Column
           key={index}
           field={col.field}
           header={col.header}
+          editor={col.fnEditor} 
+          onCellEditComplete={col.fnEditComplete}
           style={col.style}
           body={col.body}
         />
       ))}
     </DataTable>
-  );
+  </section>
 }
 
 export default FixtureTable;
