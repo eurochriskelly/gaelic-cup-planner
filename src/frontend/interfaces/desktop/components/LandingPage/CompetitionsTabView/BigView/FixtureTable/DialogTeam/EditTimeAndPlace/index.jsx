ecf9196908a745a18cd355595f066fe99b541cc2
Chris Kelly <eurochriskelly@gmail.com>
2024-10-26 16:13:51 +0200

    Breaking large component into smaller parts

--- src/frontend/interfaces/desktop/components/LandingPage/CompetitionsTabView/BigView/FixtureTable/DialogTeam/EditTimeAndPlace/index.jsx
+++ src/frontend/interfaces/desktop/components/LandingPage/CompetitionsTabView/BigView/FixtureTable/DialogTeam/EditTimeAndPlace/index.jsx
@@ -1,21 +1,40 @@
 import { useState } from 'react';
 import { Calendar } from 'primereact/calendar';
-import { ListBox } from 'primereact/listbox';
+import { Dropdown } from 'primereact/dropdown';
 
-function EditTimeAndPlace() {
-    const [selectedTime, setSelectedTime] = useState(new Date());
-    const [selectedCity, setSelectedCity] = useState(null);
-    const cities = [
-        { name: 'New York', code: 'NY' },
-        { name: 'Rome', code: 'RM' },
-        { name: 'London', code: 'LDN' },
-        { name: 'Istanbul', code: 'IST' },
-        { name: 'Paris', code: 'PRS' }
+function EditTimeAndPlace(props) {
+    const {
+        pitches = ['Pitch 1', 'Pitch 2', 'Pitch 3'],
+        currPitch = 'Pitch 1',
+        fixtures = [
+            {
+                matchId: 1,
+                startTime: '10:00',
+                team1: 'Team 1',
+                team2: 'Team 2',
+            },
+            {
+                matchId: 2,
+                startTime: '12:00',
+                team1: 'Team 4',
+                team2: 'Team 2',
+            }
+        ],
+    } = props;
+    const actions = [
+        'Move before fixture',
+        'Move after fixture',
+        'Swap with fixture'
     ];
+    const [selectedTime, setSelectedTime] = useState(new Date());
+    const [selectedPitch, setSelectedPitch] = useState(currPitch);
+    const [selectedAction, setSelectedAction] = useState(actions[0]);
+    const [selectedFixture, setSelectedFixture] = useState(null);
     return (
         <div>
             <h3>Start time</h3>
             <Calendar
+                className='w-full md:w-14rem'
                 value={selectedTime}
                 onChange={(e) => setSelectedTime(e.value)}
                 timeOnly
@@ -23,23 +42,52 @@ function EditTimeAndPlace() {
                 showTime
                 dateFormat="HH:mm"
             />
-            <h3>Location</h3>
-            <ListBox
-                value={selectedCity}
-                onChange={(e) => setSelectedCity(e.value)}
-                options={cities}
-                optionLabel="name"
+            <div>
+                <div className='w-full md:w-14rem text-center m-2'>OR..</div>
+                <div>
+                    <Dropdown
+                        className='w-full md:w-14rem'
+                        placeholder='Select action'
+                        value={selectedAction}
+                        onChange={(e) => setSelectedAction(e?.value)}
+                        options={actions.map(a => ({ label: a, value: a }))}
+                    />
+                </div>
+                <div>
+                    <Dropdown
+                        className='w-full md:w-14rem'
+                        placeholder='Select fixture'
+                        value={selectedFixture}
+                        onChange={(e) => {
+                            setSelectedFixture(e?.value)
+                        }}
+                        options={fixtures.map(f => ({
+                            label: `M${f.matchId}@${f.startTime} ${f.team1} vs ${f.team2}`,
+                            value: f.matchId,
+                        }))}
+                    />
+                </div>
+            </div>
+            <h3>Pitch</h3>
+            <Dropdown
+                value={selectedPitch}
+                onChange={(e) => {
+                    console.log(`this is the value: ${e}`)
+                    setSelectedPitch(e?.value)
+                }}
+                options={pitches.map(p => ({ label: p, value: p }))}
+                placeholder="Select a pitch"
                 className="w-full md:w-14rem"
             />
+            <pre>
+                selected time: {selectedTime.toString()}
+                <br />
+                selected pitch: {selectedPitch}
+                <br />
+                selected action: {selectedAction}
+            </pre>
         </div>
     );
 }
 
 export default EditTimeAndPlace;
-
-/*
-
-In many ways, running a league is a simpler problem to crack. You have known teams with a couple of games per day. Tracking 120 matches in a day is a more interesting puzzle to solve. 
-
-By the way, I don't know if I mentioned this to you before, but when I did the ref course with you in Leuven, I was dead impressed by the ref reporting system you had created. I believe we think quite similarly and there aren't many like us out here. It might take a few goes 
-*/
