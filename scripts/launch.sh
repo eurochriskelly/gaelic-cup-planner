efe653a10abc887a1eef17f47ef6a78f4d8e80ae
Chris Kelly <eurochriskelly@gmail.com>
2024-11-02 15:23:32 +0100

    Put dev app on separate port range

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 cleanup() {
-	echo "Cleaning up ..."
-  pkill -P $$ 
+  echo "Cleaning up ..."
+  pkill -P $$
 }
 
 trap cleanup EXIT TERM INT
 
-
 runApp() {
-	local run_number=0
-	local pid=$2
+  local run_number=0
+  local pid=$2
   local CMD=node
   if [ "$DEV_MODE" = true ]; then
     CMD=nodemon
@@ -20,43 +19,45 @@ runApp() {
     CMD=nodemon
   fi
 
-	local relaunch=true
-	while "$relaunch"; do
-		stopFile=/tmp/STOP_${pid}
-		if [ -f "$stopFile" ]; then
-			relaunch=false
-		else
+  local relaunch=true
+  while "$relaunch"; do
+    stopFile=/tmp/STOP_${pid}
+    if [ -f "$stopFile" ]; then
+      relaunch=false
+    else
       echo "Run # ${run_number}"
-			$CMD src/backend/server.js --port="$2" --app="$1" --use-mock=$DEV_MODE
-			run_number=$((run_number + 1))
-		fi
-	done
+      $CMD src/backend/server.js --port="$2" --app="$1" --use-mock=$DEV_MODE
+      run_number=$((run_number + 1))
+    fi
+  done
   return
 }
 
-if [ ! -f gg_env.sh ];then
+if [ ! -f gg_env.sh ]; then
   echo "Error: enviroment file not found [gg_env.sh]"
   exit 1
+else
+  source ./gg_env.sh
 fi
 
 DEV_MODE=false
 for arg in "$@"; do
-    if [ "$arg" = "--port" ]; then
-        shift
-        PORT=$1
-        shift
-    fi
-    if [ "$arg" = "--app" ]; then
-        shift
-        APP=$1 
-        shift
-    fi
-    if [ "$arg" = "--dev" ]; then
-        DEV_MODE=true
-    fi
-    if [ "$arg" = "--watch" ]; then
-        WATCH_MODE=true
-    fi
+  if [ "$arg" = "--port" ]; then
+    shift
+    PORT=$1
+    shift
+  fi
+  if [ "$arg" = "--app" ]; then
+    shift
+    APP=$1
+    shift
+  fi
+  if [ "$arg" = "--dev" ]; then
+    DEV_MODE=true
+  fi
+  if [ "$arg" = "--watch" ]; then
+    WATCH_MODE=true
+  fi
 done
 runApp "$APP" "$PORT" &
 
