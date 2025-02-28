50281e1bb046154f0ba3e4cc19cdcffd1b69e938
Chris Kelly <eurochriskelly@gmail.com>
2025-02-28 16:13:05 +0100

    Style an image on the landing page

--- src/frontend/interfaces/mobile/components/LandingPage/LandingPage.hooks.jsx
+++ src/frontend/interfaces/mobile/components/LandingPage/LandingPage.hooks.jsx
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react';
 export const useFetchTournament = (tid) => {
   const [tournInfo, setTournInfo] = useState({});
   useEffect(() => {
+    if (!tid) return; // Add this line
     fetch(`/api/tournaments/${tid}`)
       .then((response) => response.json())
       .then((data) => setTournInfo(data.data))
       .catch((error) => {
         console.error("Error fetching tournament info:", error);
       });
-  }, []);
+  }, [tid]);
 
   return { tournInfo }
 }
