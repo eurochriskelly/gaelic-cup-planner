c7383e87b9219fb30e1e9c66e6e42fa429989281
Chris Kelly <eurochriskelly@gmail.com>
2025-03-16 21:30:22 +0100

    Cleaner name display

--- src/frontend/shared/generic/TeamNameDisplay/index.jsx
+++ src/frontend/shared/generic/TeamNameDisplay/index.jsx
@@ -1,32 +1,58 @@
 import { Schema } from "js-yaml"
 
 export const formatTeamName = (team) => {
-  let displayTeam = team
-  if (team === '~') return ''
+  let displayTeam = team;
+  if (team === '~') return '';
+
   if (team?.startsWith('~')) {
     // e.g. "~match:102/p:1"
-    const parts = team?.replace('~', '').split('/')
-    
-    const dependent = parts[0]
-    const position = +(parts[1].replace('p:', ''))
-    const [ type, order ] = dependent.split(':')
-    const outcome = order === 1 ? 'Winner' : 'Loser'
+    const parts = team?.replace('~', '').split('/');
+
+    const dependent = parts[0];
+    const position = +(parts[1].replace('p:', ''));
+    const [type, order] = dependent.split(':');
+    const outcome = order === 1 ? 'Winner' : 'Loser';
+
     switch (type) {
       case 'match':
-        displayTeam = `${outcome} of FIXTURE #${position}`
-        break
+        displayTeam = `${outcome} of FIXTURE #${position}`;
+        break;
       case 'semis':
       case 'finals':
       case 'quarters':
-        displayTeam = `${outcome} of ${type.toUpperCase()} #${position}`
-        break
-      default: 
-        displayTeam =  'T.B.D.'
-        break
+        displayTeam = `${outcome} of ${type.toUpperCase()} #${position}`;
+        break;
+      default:
+        displayTeam = 'T.B.D.';
+        break;
     }
   }
-  return displayTeam.toUpperCase();
-}
+
+  return formatWithSpans(displayTeam.toUpperCase());
+};
+
+const formatWithSpans = (text) => {
+  if (text === 'T.B.D.') {
+    return <span className="text-rose-500">T.B.D.</span>;
+  }
+
+  if (text.includes('/')) {
+    return (
+      <span>
+        {text.split(/(\/)/).map((part, index) =>
+          part === '/' ? (
+            <span key={index} className="slash text-rose-500 ml-2 mr-2"> / </span>
+          ) : (
+            <span key={index} className="whitespace-nowrap">{part}</span>
+          )
+        )}
+      </span>
+    );
+  }
+
+  return <span>{text}</span>;
+};
+
 
 export const militaryTimeDiffMins = (startTime, endTime) => {
   if (startTime && endTime) {
