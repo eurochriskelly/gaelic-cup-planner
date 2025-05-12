b7b299c26df3faeb7884b6ea6872b249e1edce04
Chris Kelly <eurochriskelly@gmail.com>
2025-05-12 19:10:41 +0200

    adding styled images

--- src/shared/generic/LoginHeader/index.jsx
+++ src/shared/generic/LoginHeader/index.jsx
@@ -1,15 +1,19 @@
 import './LoginHeader.scss'; // Import the component's styles
-import whistleImage from '../../../interfaces/mobile/public/pp-whistle.png';
+import whistleImage from '../../../interfaces/mobile/public/images/banner.png';
 
 const LoginHeader = ({ version, showBackButton, onBackClick }) => {
   const isRC = (version.includes('_RC'));
   return (
     <div className="app-header">
-      <div className="logo-container">
-        <img src={whistleImage} className="whistle" alt="Pitch Perfect Whistle" />
-      </div>
+      <div className="logo-container">{
+        /* <img src={whistleImage} className='whistle' /> */
+      }</div>
       <div className="title-group">
-        <span className="app-name">Pitch Perfect</span>
+        {showBackButton && (
+          <span className="back-icon-span" onClick={onBackClick}>
+            <i className="pi pi-arrow-circle-left"></i>
+          </span>
+        )}
         <div className="version-line">{
           isRC
             ? <span style={{ color: 'red', whiteSpace: 'nowrap', fontWeight: 'bold' }} className="app-version">
@@ -19,11 +23,6 @@ const LoginHeader = ({ version, showBackButton, onBackClick }) => {
             : <span className="app-version">v{version || '?.?.?'}</span>
         }</div>
       </div>
-      {showBackButton && (
-        <span className="back-icon-span" onClick={onBackClick}>
-          <i className="pi pi-arrow-circle-left"></i>
-        </span>
-      )}
     </div>
   )
 };
--- src/shared/js/Provider.jsx
+++ src/shared/js/Provider.jsx
@@ -4,8 +4,8 @@ import Cookies from "js-cookie";
 const Context = createContext();
 
 const versionInfo = {
-  mobile: "%%0.4.21_RC%%".replace(/%/g, ''),
-  desktop: "%%0.0.20_RC%%".replace(/%/g, ''),
+  mobile: "%%0.4.22_RC%%".replace(/%/g, ''),
+  desktop: "%%0.0.21_RC%%".replace(/%/g, ''),
 };
 
 export const Provider = ({ children }) => {
