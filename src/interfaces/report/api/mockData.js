50d05e087e9de7c41a93c6338eba4f58a0e38a9f
Chris Kelly <eurochriskelly@gmail.com>
2025-05-18 20:52:44 +0200

    refactor: update mock api

---
+++ src/interfaces/report/api/mockData.js
@@ -0,0 +1,12 @@
+const tournaments = [
+  { id: 26, title: "Eindhoven Football Round 3", date: "2025-05-16", location: "Eindhoven", link: "bec22312-ee53-4c8d-b395-a633b6859e96" },
+  // ...all other tournament entries
+];
+
+const user = {
+  id: 1,
+  name: "John Doe",
+  email: "chris@pitchperfect.eu.com",
+};
+
+export { tournaments, user };
