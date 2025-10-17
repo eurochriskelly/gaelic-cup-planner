cf5120503dbaf1e7f7bdae8f840176c64cc472d9
Chris Kelly <eurochriskelly@gmail.com>
2025-10-17 13:38:18 +0200

    bump the version

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -5,7 +5,7 @@ FILE="src/shared/js/Provider.jsx"
 if [[ "$1" == "--release" ]]; then
   sed -i -E 's/%%([0-9]+)\.([0-9]+)\.([0-9]+)_RC%%/%%\1.\2.\3%%/g' "$FILE"
 elif [[ "$1" == "--release-candidate" ]]; then
-  perl -i -pe 's/%%(\d+)\.(\d+)\.(\d+)%%/"%%$1.$2." . ($3+1) . "_RC%%"/e' "$FILE"
+  perl -i -pe 's/%%(\d+)\.(\d+)\.(\d+)(_RC)?%%/"%%$1.$2." . ($3+1) . "_RC%%"/e' "$FILE"
 else
   echo "Usage: $0 --release | --release-candidate"
   exit 1
